eff4baa2545ca4bee159b71e3985edc3532eb97d
Briana <blomori@ucdavis.edu>
2020-02-21 13:17:02 -0800

    added js file

--- des157-wi20/studio2/script.js
+++ des157-wi20/studio2/script.js
@@ -3,3 +3,45 @@
     STUDIO 2: JS CODE
     WINTER 2020 
 */
+
+(function(){
+	
+	"use strict";
+	
+	const selected = document.querySelectorAll('.selected');
+
+	for( let i=0; i<selected.length; i++){
+		
+		//mouse enter event for desktop
+		selected[i].addEventListener("mouseenter", function(event){
+				event.preventDefault(); //needed to keep other events from registering
+				event.target.setAttribute("class", "selected hover-selected");
+		});
+		
+		//mouse leave event for desktop
+		selected[i].addEventListener("mouseleave", function(event){
+			event.preventDefault(); //needed to keep other events from registering
+			event.target.setAttribute("class", "selected");
+		});
+		
+		//handles touch
+		selected[i].addEventListener("touchstart", function(event){
+			event.preventDefault(); //needed to keep other events from registering
+			
+			//if the element has been 'touched', close it
+			if(event.target.classList.contains("hover-selected")){
+				event.target.setAttribute("class", "selected");
+			}
+			// if the element has not been touched, close them all then open
+			// the one that was touched.
+			else{
+				// loop through and make sure all are closed.
+				for(let i=0; i<selected.length; i++){
+					selected[i].setAttribute("class", "selected");
+				}
+				//open the one that was touched
+				event.target.setAttribute("class", "selected hover-selected");
+			}
+		});
+	}//end for loop
+}());// end closure
